52ebb168cfcbcc66cf52fff76b8e9f1f9380dd05
Naz-k Vincent <nazkvincent@gmail.com>
2024-12-24 16:56:02 -0800

    Commit

--- library_api/app/main.py
+++ library_api/app/main.py
@@ -1,2 +1,12 @@
 from fastapi import FastAPI
 from .routers import users, books, borrow_records
+
+app = FastAPI()
+
+app.include_router(books.router, prefix="/api/v1/books", tags=["Books Routes"])
+app.include_router(users.router, prefix="/api/v1/users", tags=["Users Routes"])
+app.include_router(borrow_records.router, prefix="/api/v1/borrow-records", tags=["Borrow Records Routes"])
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the Library API"}
